8d33ecaec7ac8578b0c96ae7f9d3bb030c5d5417
CedrickMQ <112716158+CedrickMQ@users.noreply.github.com>
2023-05-01 00:10:13 -0600

    Se agregaron mas acciones a botones y nuevas pantallas con texto

--- app/src/main/java/com/example/screenbreak/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/screenbreak/ui/gallery/GalleryFragment.java
@@ -21,16 +21,33 @@ import com.example.screenbreak.ui.gallery.Notas.Nota1Fragment;
 public class GalleryFragment extends Fragment {
 
     private Button button;
+    private Button button2;
+    private Button button3;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
 
         button = root.findViewById(R.id.button1);
+        button2 = root.findViewById(R.id.button2);
+        button3 = root.findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Navigation.findNavController(v).navigate(R.id.action_nav_gallery_to_Nota1);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_nav_gallery_to_Nota2);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_nav_gallery_to_Nota3);
             }
         });
 
